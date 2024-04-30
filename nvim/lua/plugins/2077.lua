d60ab8c6e2a99af3929a5736fe13f459475ed3d9
sirus20x6 <sirus20x6@users.noreply.github.com>
2024-04-30 16:16:33 -0500

    neovim files

---
+++ nvim/lua/plugins/2077.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "akai54/2077.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("2077")
+        end,
+    },
+}
